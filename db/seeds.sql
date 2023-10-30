eb6ec96d2993f92271802681b56758a7794e1f89
nathangero <nathangero@yahoo.com>
2023-10-30 15:18:37 -0700

    update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO department (name)
 VALUES 	("Electronics"),
 		("Kids clothing"),
-        ("Floor"),
+        ("Snacks"),
         ("Cameras");
        
 INSERT INTO role (title, salary, department_id)
@@ -10,11 +10,11 @@ VALUES 	("Manager", 20.00, 1),
         ("Manager", 18.00, 2),
         ("Manager", 18.00, 3),
         ("Specialist", 17.00, 2),
-        ("Specialist", 17.00, 3);
+        ("Floor Associate", 12.00, 3);
         
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES 	("Nathan", "Geronimo", 2, 2),
+VALUES 	("Nathan", "Geronimo", 2, 1),
 		("Emily", "Vincent", 1, NULL),
-        ("Chris", "Evans", NULL, 3),
+        ("Chris", "Evans", NULL, 2),
         ("Robert", "Downy", NULL, NULL);
         
